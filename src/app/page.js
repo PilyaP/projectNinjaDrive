e85732d524063c93e6d80cd9c871c20862241b9e
PilyaP <ppaexpert@gmail.com>
2024-05-29 02:20:48 +0300

    certificate

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
       {/* <Services /> */}
       <Pricing />
       <Advantages />
-      {/* <GiftCertificate /> */}
+      <GiftCertificate />
       {/* <Reviews /> */}
       <Contacts />
       <Footer />
