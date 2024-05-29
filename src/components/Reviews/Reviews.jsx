108a47680403c8a0a34fb17c2d9ecb260bb0e54a
PilyaP <ppaexpert@gmail.com>
2024-05-30 02:08:24 +0300

    review

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,27 +1,30 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Reviews.module.scss";
-import Image from "next/image";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const reviews = [
   {
-    name: "Инна",
-    text: "Чудовый клуб! Ингивидуальный пидхид, нова и дуже крута потужна техника. Море позитивных эмоций, гарний маршрут лисом. Рекомендую",
+    name: "Інна",
+    text: "Чудовий клуб! Індивідуальний підхід, нова і дуже крута потужна техніка😊👍   Море позитивних емоцій, гарний маршрут лісом 😊Рекомендую.",
     source: "*Видгук з гугл карт",
-    initials: "I",
+    initials: "І",
     avatar: "/path/to/avatar1.png",
   },
   {
     name: "Владислав",
-    text: "Были килька разив. Все видминно, биг атмосфери, сервиси момив и го маршрутів! Крути инструктори, купа шикарних эмоций!",
+    text: "Були кілька разів. Все відмінно, від атмосфери, сервісу мотів і до маршрутів! Круті інструктори, купа шикарних емоцій!",
     source: "*Видгук з гугл карт",
     initials: "B",
     avatar: "/path/to/avatar2.png",
   },
   {
     name: "Ярослав",
-    text: "Найкращий прокат мото-техники, є багато квадроциклів, ендуро, багги. Вся техника дуже потужна и обслужена.",
+    text: "Найкращий прокат мото-техніки, є багато квадроциклів, ендуро, баггі.  Вся техніка дуже потужна і обслужена",
     source: "*Видгук з гугл карт",
     initials: "Я",
     avatar: "/path/to/avatar3.png",
@@ -50,7 +53,6 @@ const reviews = [
 ];
 
 const Reviews = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
   const getRandomColor = () => {
     const letters = "0123456789ABCDEF";
     let color = "#";
@@ -60,22 +62,14 @@ const Reviews = () => {
     return color;
   };
 
-  const nextSlide = () => {
-    if (currentIndex < reviews.length - 3) {
-      setCurrentIndex(currentIndex + 1);
-    }
-  };
-
-  const prevSlide = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
-    }
-  };
-
   return (
     <section id="reviews" className={styles.reviewsSection}>
       <div className="container">
-        <h2 className={styles.title}>що говорять про нас люди</h2>
+        <h2 className={styles.title}>Відгуки</h2>
+        <h3 className={styles.titleDesk}>що говорять про нас люди</h3>
+        <p className={styles.subtitleReview}>
+          Відгуки тих, хто скористався нашими послугами, говорять самі за себе
+        </p>
         <div className={styles.subtitle}>
           <p>
             Ми завжди прагнемо досконалості у всьому, що робимо, і наші клієнти
@@ -87,42 +81,55 @@ const Reviews = () => {
             індивідуальному підході до кожного замовлення
           </p>
         </div>
-        <div className={styles.imageReviewPosition}>
-          <div className={styles.imageReview}>
-            <Image
-              src="/rewiev-desk.png"
-              alt="rewiev"
-              width={410}
-              height={460}
-            />
-          </div>
-        </div>
-        <div className={styles.slider}>
-          <button onClick={prevSlide} className={styles.arrow}>
-            &#8249;
-          </button>
-          <div className={styles.reviewContainer}>
-            {reviews
-              .slice(currentIndex, currentIndex + 3)
-              .map((review, index) => (
-                <div key={index} className={styles.reviewCard}>
-                  <div
-                    className={styles.avatar}
-                    style={{ backgroundColor: getRandomColor() }}
-                  >
-                    <span className={styles.initials}>{review.initials}</span>
-                  </div>
-                  <div className={styles.reviewContent}>
-                    <h3 className={styles.name}>{review.name}</h3>
-                    <p className={styles.text}>{review.text}</p>
-                    <p className={styles.source}>{review.source}</p>
-                  </div>
-                </div>
-              ))}
-          </div>
-          <button onClick={nextSlide} className={styles.arrow}>
-            &#8250;
-          </button>
+
+        <div
+          data-aos="fade-up-right"
+          data-aos-duration="1000"
+          data-aos-once="true"
+        >
+          <Swiper
+            pagination={{ clickable: true }}
+            modules={[Pagination]}
+            spaceBetween={30}
+            breakpoints={{
+              480: {
+                slidesPerView: 1,
+              },
+
+              768: {
+                slidesPerView: 2,
+              },
+
+              1200: {
+                slidesPerView: 3,
+              },
+            }}
+            className="mySwiper"
+          >
+            {reviews.map((review, index) => (
+              <SwiperSlide key={index}>
+                <ul className={styles.reviewContainer}>
+                  <li className={styles.reviewCard}>
+                    <div className={styles.avatarName}>
+                      <div
+                        className={styles.avatar}
+                        style={{ backgroundColor: getRandomColor() }}
+                      >
+                        <span className={styles.initials}>
+                          {review.initials}
+                        </span>
+                      </div>
+                      <h3 className={styles.name}>{review.name}</h3>
+                    </div>
+                    <div className={styles.reviewContent}>
+                      <p className={styles.text}>{review.text}</p>
+                      <p className={styles.source}>{review.source}</p>
+                    </div>
+                  </li>
+                </ul>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </section>
